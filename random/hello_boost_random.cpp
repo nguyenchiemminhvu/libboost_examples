418a62034cc91480115905da7def6750f61bf309
vu3.nguyen <vu3.nguyen@lge.com>
2024-09-23 13:29:54 +0900

    hello boost random

--- random/hello_boost_random.cpp
+++ random/hello_boost_random.cpp
@@ -5,11 +5,34 @@ int main()
 {
     boost::random::mt19937 rng;
     rng.seed(static_cast<unsigned int>(std::time(0)));
-    boost::random::uniform_int_distribution<> int_dist(0, 100);
 
+    boost::random::uniform_int_distribution<> int_dist(0, 100);
     for (int i = 0; i < 10; i++)
     {
-        std::cout << int_dist(rng) << std::endl;
+        std::cout << int_dist(rng) << " ";
     }
+    std::cout << std::endl;
+
+    boost::random::normal_distribution<> normal_dist(0.0, 1.0);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << normal_dist(rng) << " ";
+    }
+    std::cout << std::endl;
+
+    boost::random::binomial_distribution<> binomial_dist(10, 0.5);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << binomial_dist(rng) << " ";
+    }
+    std::cout << std::endl;
+
+    boost::random::bernoulli_distribution<> bool_dist(0.5);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << bool_dist(rng) << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
