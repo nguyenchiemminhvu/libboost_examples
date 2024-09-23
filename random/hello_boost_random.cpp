2d1bbb4d449e771f2b3a3d77ec72c21c1c3f3003
vu3.nguyen <vu3.nguyen@lge.com>
2024-09-23 13:19:50 +0900

    update boost random

--- random/hello_boost_random.cpp
+++ random/hello_boost_random.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <boost/random.hpp>
 
 int main()
 {
+    boost::random::mt19937 rng;
+    rng.seed(static_cast<unsigned int>(std::time(0)));
+    boost::random::uniform_int_distribution<> int_dist(0, 100);
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << int_dist(rng) << std::endl;
+    }
     return 0;
 }
