f17f951617f7e112b3447216172af750188b3225
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-24 16:37:30 +0000

    boost json pretty print example

--- json/json_build_json_from_empty.cpp
+++ json/json_build_json_from_empty.cpp
@@ -3,6 +3,103 @@
 #include <cstdint>
 #include <boost/json.hpp>
 
+void pretty_print(std::ostream& os, boost::json::value& j_val, std::string* indent = nullptr)
+{
+    std::string local_indent;
+    if (indent == nullptr)
+    {
+        indent = &local_indent;
+    }
+
+    switch (j_val.kind())
+    {
+        case boost::json::kind::object:
+        {
+            os << "{\n";
+            indent->append(4, ' ');
+
+            boost::json::object obj = j_val.get_object();
+            if (!obj.empty())
+            {
+                boost::json::object::iterator iter = obj.begin();
+                do
+                {
+                    os << *indent << iter->key() << " : ";
+                    pretty_print(os, iter->value(), indent);
+                    iter++;
+                    if (iter == obj.end())
+                        break;
+                    os << ",\n";
+                } while (iter != obj.end());
+            }
+
+            os << "\n";
+            indent->resize(indent->size() - 4);
+            os << *indent << "}";
+            break;
+        }
+        case boost::json::kind::array:
+        {
+            os << "[\n";
+            indent->append(4, ' ');
+
+            boost::json::array arr = j_val.get_array();
+            if (!arr.empty())
+            {
+                boost::json::array::iterator iter = arr.begin();
+                do
+                {
+                    os << *indent;
+                    pretty_print(os, *iter, indent);
+                    iter++;
+                    if (iter == arr.end())
+                        break;
+                    os << ",\n";
+                } while (iter != arr.end());
+            }
+
+            os << "\n";
+            indent->resize(indent->size() - 4);
+            os << *indent << "]";
+            break;
+        }
+        case boost::json::kind::string:
+        {
+            os << boost::json::serialize(j_val.get_string());
+            break;
+        }
+        case boost::json::kind::double_:
+        case boost::json::kind::uint64:
+        case boost::json::kind::int64:
+        {
+            os << j_val;
+            break;
+        }
+        case boost::json::kind::bool_:
+        {
+            if (j_val.get_bool())
+            {
+                os << "true";
+            }
+            else
+            {
+                os << "false";
+            }
+            break;
+        }
+        case boost::json::kind::null:
+        {
+            os << "null";
+            break;
+        }
+    }
+
+    if (indent->empty())
+    {
+        os << "\n";
+    }
+}
+
 boost::json::object json_create_employee(const std::string& id, const std::string& name, int age, int grade)
 {
     boost::json::object emp;
@@ -15,16 +112,27 @@ boost::json::object json_create_employee(const std::string& id, const std::strin
 
 int main()
 {
-    boost::json::object root;
+    boost::json::value j_val;
+    {
+        boost::json::object root;
 
-    boost::json::array employees;
-    employees.push_back(json_create_employee("vu3.nguyen", "Nguyen Chiem Minh Vu", 30, 3));
-    employees.push_back(json_create_employee("truong.le", "Le Nhat Truong", 25, 3));
-    employees.push_back(json_create_employee("hieu6.nguyen", "Nguyen Minh Hieu", 25, 2));
-    
-    root["employees"] = employees;
-    
-    std::cout << boost::json::serialize(root) << std::endl;
+        root["Company"] = "LG Electronics R&D Vietnam";
+
+        boost::json::object owner = json_create_employee("jongwook02.lee", "Lee Jong Wook", 50, 5);
+        root["Owner"] = owner;
+
+        boost::json::array employees;
+        employees.push_back(json_create_employee("vu3.nguyen", "Nguyen Chiem Minh Vu", 30, 3));
+        employees.push_back(json_create_employee("truong.le", "Le Nhat Truong", 25, 3));
+        employees.push_back(json_create_employee("hieu6.nguyen", "Nguyen Minh Hieu", 25, 2));
+        root["employees"] = employees;
+
+        j_val = root;
+    }
+
+    std::cout << boost::json::serialize(j_val) << std::endl;
+
+    pretty_print(std::cout, j_val);
 
     return 0;
 }
