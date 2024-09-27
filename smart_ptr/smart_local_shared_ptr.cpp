70a6da0ae95a4efa5a0749628a38105c341c317c
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-27 07:58:34 +0000

    update

--- smart_ptr/smart_local_shared_ptr.cpp
+++ smart_ptr/smart_local_shared_ptr.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <boost/smart_ptr/local_shared_ptr.hpp>
+
+class Sample
+{
+public:
+    Sample()
+    {
+        std::cout << "Sample constructor" << std::endl;
+    }
+
+    ~Sample()
+    {
+        std::cout << "Sample destructor" << std::endl;
+    }
+
+    void do_something()
+    {
+        std::cout << "Sample do something" << std::endl;
+    }
+};
 
 int main()
 {
+    boost::local_shared_ptr<Sample> local_ptr(new Sample());
+    boost::local_shared_ptr<Sample> another_local_ptr = local_ptr;
+    std::cout << local_ptr.local_use_count() << std::endl;
+
     return 0;
 }
