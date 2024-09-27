cddca11df421b1650e315356dcfdcfb011f24805
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-27 05:14:41 +0000

    boost scope array

--- smart_ptr/smart_scoped_array.cpp
+++ smart_ptr/smart_scoped_array.cpp
@@ -3,5 +3,11 @@
 
 int main()
 {
+    boost::scoped_array<int> data(new int[5]);
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
