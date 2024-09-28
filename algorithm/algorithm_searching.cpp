942d7a73361ce51263471d9e9e5185a7e58998ff
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-09-28 13:25:21 +0000

    refactoring

--- algorithm/algorithm_searching.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <boost/algorithm/searching/boyer_moore.hpp>
-#include <boost/algorithm/searching/knuth_morris_pratt.hpp>
-#include <boost/algorithm/searching/boyer_moore_horspool.hpp>
-
-int main()
-{
-    return 0;
-}
